45f069ea39ff5e16c6cda976d9971859613aaef3
Dingo <oferdottan@gmail.com>
2016-10-23 13:55:07 +0300

    Tabbed format

--- Source/WorkGiver_CCRefuel.cs
+++ Source/WorkGiver_CCRefuel.cs
@@ -3,14 +3,14 @@ using RimWorld;
 
 namespace CCRefuel
 {
-    public class WorkGiver_CCRefuel : WorkGiver_Refuel
-    {
-        public override ThingRequest PotentialWorkThingRequest
-        {
-            get
-            {
-                return ThingRequest.ForDef(ThingDef.Named("FueledStove"));
-            }
-        }
-    }
+	public class WorkGiver_CCRefuel : WorkGiver_Refuel
+	{
+		public override ThingRequest PotentialWorkThingRequest
+		{
+			get
+			{
+				return ThingRequest.ForDef(ThingDef.Named("FueledStove"));
+			}
+		}
+	}
 }
