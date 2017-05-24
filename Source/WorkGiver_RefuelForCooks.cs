dd2495c4cb0fc0918b101ed989d27d3ec1cbb791
Dingo <oferdottan@gmail.com>
2017-05-25 02:53:15 +0300

    A17 release
    Compiled using latest A17 syntax.

--- Source/WorkGiver_RefuelForCooks.cs
+++ Source/WorkGiver_RefuelForCooks.cs
@@ -5,14 +5,9 @@ namespace CooksCanRefuel
 {
 	public class WorkGiver_RefuelForCooks : WorkGiver_Refuel
 	{
-		public override bool HasJobOnThing(Pawn pawn, Thing t)
+		public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
 		{
 			return base.HasJobOnThing(pawn, t) && t.def.building.isMealSource;
-		}
-
-		public override bool HasJobOnThingForced(Pawn pawn, Thing t)
-		{
-			return base.HasJobOnThingForced(pawn, t) && t.def.building.isMealSource;
 		}
 	}
 }
