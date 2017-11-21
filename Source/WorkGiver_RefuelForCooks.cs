ee14d6dfac2d5c8a25f44ef5ad2d00d52cccb901
Dingo <oferdottan@gmail.com>
2017-11-21 06:36:16 +0200

    Revert workgiver change

--- Source/WorkGiver_RefuelForCooks.cs
+++ Source/WorkGiver_RefuelForCooks.cs
@@ -1,15 +1,13 @@
-using System.Collections.Generic;
 using RimWorld;
 using Verse;
 
-namespace Cooks_Can_Refuel
+namespace CooksCanRefuel
 {
 	public class WorkGiver_RefuelForCooks : WorkGiver_Refuel
 	{
-		//RimWorld.JobGiver_Work.TryIssueJobPackage
-		public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+		public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
 		{
-			return pawn.Map.listerThings.ThingsMatching(this.PotentialWorkThingRequest).FindAll(t => t.def.building.isMealSource);
+			return t.def.building.isMealSource && base.HasJobOnThing(pawn, t, forced);
 		}
 	}
 }
